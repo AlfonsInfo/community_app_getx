5e7a37ceb62e06e62d33e7e1343868f3c42388f8
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-12-08 01:38:19 +0700

    fix localizations validations

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,116 +1,113 @@
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_en.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_id.dart';
 import 'package:jdlcommunity_getx/app/constants/constants.dart';
 import 'package:jdlcommunity_getx/app/modules/abstract/controller_abstract.dart';
 import 'package:jdlcommunity_getx/app/routes/app_pages.dart';
 import 'package:jdlcommunity_getx/app/utils/logging_utils.dart';
+
 // import 'package:jdlcommunity_getx/app/utils/logging_utils.dart';
 class LoginController extends GetxController implements GetxControllerAbstract {
-
   RxBool isIndoLanguageToggle = RxBool(true);
   RxBool isEyeToggleHideItem = RxBool(true);
   RxInt firstChanges = 1.obs;
 
   RxString username = RxString("");
-  RxnString errorTextUsername = RxnString();  
+  RxnString errorTextUsername = RxnString();
 
   RxString password = RxString("");
   RxnString errorTextPassword = RxnString();
   final formKey = GlobalKey<FormState>().obs;
 
   RxBool isEverFocusedUsername = RxBool(false);
-  RxBool isEverFocusedPassword= RxBool(false);
-  
+  RxBool isEverFocusedPassword = RxBool(false);
 
-  usernameChanged(value){
+  final englishLang = AppLocalizationsEn();
+  final indoLang = AppLocalizationsId();
+
+  usernameChanged(value) {
     username.value = value;
     validateUsername();
   }
-  passwordChanged(value){
+
+  passwordChanged(value) {
     password.value = value;
-      validatePassword();
+    validatePassword();
   }
-  toggleLanguage(){
-    Locale sebelumLocale = Get.locale!;
-    LoggingUtils.logDebugValue(sebelumLocale.toString(), "SEBELUM");
 
+  toggleLanguage() {
     isIndoLanguageToggle.toggle();
-
-
-    BuildContext context2 = Get.context!;
-    Locale sesudahLocale = Get.locale!;
-    Locale sesudahDariContext = Localizations.localeOf(context2);
-    LoggingUtils.logDebugValue(sesudahLocale.toString(), "locale cek 2");
-    LoggingUtils.logDebugValue(sesudahDariContext.toString(), "locale sesudah dari context");
-    
-    
-    LoggingUtils.logDebugValue(AppLocalizations.of(context2).empty_validate(""), "") ;
-    validateUsername(context: context2);
-
+    if (isEverFocusedUsername.value || isEverFocusedPassword.value) {
+      validateUsername();
+      validatePassword();
+    }
   }
+
   @override
   toggleEye(RxBool value) => value.toggle();
   checkEverFocusedOnUsername() {
     isEverFocusedUsername.value = true;
   }
-  checkEverFocusedOnPassword(){      
+
+  checkEverFocusedOnPassword() {
     isEverFocusedPassword.value = true;
-    
   }
-
-  // Rx<AppLocalizations?> appLocalizations = Rx<AppLocalizations?>(AppLocalizations.of(Get.context!));
-
 
   @override
   void onInit() {
-      ever(isIndoLanguageToggle, (_) {  
-        //validateUsername(context: context);
-        //validatePassword(context: context);
-    });
+    ever(isIndoLanguageToggle, (_) {});
     super.onInit();
   }
 
-
-
-  validateUsername({BuildContext? context})
-  {
-    CommonConditions.validateNotEmptyAndNotContainSpace(username.value) 
-    ? errorTextUsername.value = AppLocalizations.of(context ?? Get.context!).empty_validate(AppLocalizations.of(Get.context!).username)
-    : errorTextUsername.value =  null;
+  validateUsername({BuildContext? context}) {
+    Locale currentLocale = Get.locale!;
+    if (CommonConditions.validateNotEmptyAndNotContainSpace(username.value)) {
+      errorTextUsername.value = isEnglish(currentLocale)
+          ? englishLang.empty_validate(englishLang.username)
+          : indoLang.empty_validate(indoLang.username);
+    } else {
+      errorTextUsername.value = null;
+    }
   }
 
+  bool isEnglish(Locale currentLocale) => currentLocale.toString() == "en";
 
-  validatePassword({BuildContext? context} ){
-    if(CommonConditions.validateNotEmptyAndNotContainSpace(password.value))
-    {
-      errorTextPassword.value =  AppLocalizations.of(context ?? Get.context!).empty_validate(AppLocalizations.of(Get.context!).password);
+  validatePassword({BuildContext? context}) {
+    Locale currentLocale = Get.locale!;
+    if (CommonConditions.validateNotEmptyAndNotContainSpace(password.value)) {
+      errorTextPassword.value = isEnglish(currentLocale)
+          ? englishLang.empty_validate(englishLang.password)
+          : indoLang.empty_validate(indoLang.password);
       return;
-    }else{
-      errorTextPassword.value =  null;
+    } else {
+      errorTextPassword.value = null;
     }
 
-    if(password.value.length < 5){
-      errorTextPassword.value =  AppLocalizations.of(Get.context!).minimum_length( "5", AppLocalizations.of(Get.context!).password);
+    if (password.value.length < 5) {
+      errorTextPassword.value = isEnglish(currentLocale)
+          ? englishLang.minimum_length(englishLang.password, 5)
+          : indoLang.minimum_length(indoLang.password, 5);
+
       return;
-    } else{
-      errorTextPassword.value =  null;
+    } else {
+      errorTextPassword.value = null;
     }
   }
 
   Future<bool> Function() submitFunction() {
     LoggingUtils.logStartFunction("SubmitFunction");
     return () async {
-      await Future.delayed(const Duration(seconds: 0), () 
-      {
+      await Future.delayed(const Duration(seconds: 0), () {
         validateUsername();
         validatePassword();
-         if(errorTextUsername.value == null && errorTextPassword.value == null){
-            Get.toNamed(Routes.home);
-         }
+        if (errorTextUsername.value == null &&
+            errorTextPassword.value == null) {
+          Get.toNamed(Routes.home);
+        }
       });
       return true;
     };
   }
 }
-
